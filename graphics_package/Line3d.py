d3cf9e7480c62041a3a9ab58ab4c5d215ceacd41
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-24 14:06:30 +0200

    Update Line3d.py

--- graphics_package/Line3d.py
+++ graphics_package/Line3d.py
@@ -1,31 +1,44 @@
-
-from .Point3d import Point3d
-from .Line2d import Line2d
-from .Transformation3d import Transformation3d
+# graphics_package/line3d.py
+from graphics_package.point3d import Point3d
+from graphics_package.vector4d import Vector4d
+from graphics_package.transformation3d import Transformation3d
+from graphics_package.line2d import Line2d
 
 class Line3d:
-    def __init__(self, x1: float, y1: float, z1: float, x2: float, y2: float, z2: float):
-        self.src = Point3d(x1, y1, z1)
-        self.dest = Point3d(x2, y2, z2)
+    def __init__(self, *args):
+        """
+        Constructors:
+        - Line3d(x1, y1, z1, x2, y2, z2)
+        - Line3d(Point3d, Point3d)
+        - Line3d(Vector4d, Vector4d)
+        """
+        if len(args) == 6:  # raw coords
+            x1, y1, z1, x2, y2, z2 = args
+            self.src = Point3d(x1, y1, z1)
+            self.dest = Point3d(x2, y2, z2)
+        elif len(args) == 2 and isinstance(args[0], Point3d):
+            self.src, self.dest = args
+        elif len(args) == 2 and isinstance(args[0], Vector4d):
+            a, b = args
+            self.src = Point3d.from_vector(a)
+            self.dest = Point3d.from_vector(b)
+        else:
+            raise TypeError("Invalid constructor arguments for Line3d")
 
-    @classmethod
-    def from_points(cls, pSrc: Point3d, pDest: Point3d) -> "Line3d":
-        return cls(pSrc.x(), pSrc.y(), pSrc.z(), pDest.x(), pDest.y(), pDest.z())
-
-    def getSourcePoint(self) -> Point3d:
+    def get_source_point(self) -> Point3d:
         return self.src
 
-    def getDestinationPoint(self) -> Point3d:
+    def get_destination_point(self) -> Point3d:
         return self.dest
 
     def line2d(self) -> Line2d:
-        src = self.getSourcePoint()
-        dest = self.getDestinationPoint()
-        return Line2d(src.x(), src.y(), dest.x(), dest.y())
+        """Project to 2D by stripping z-coordinate."""
+        return Line2d(self.src.x(), self.src.y(), self.dest.x(), self.dest.y())
 
-    def transform(self, matrix: Transformation3d) -> None:
-        self.src = matrix @ self.src
-        self.dest = matrix @ self.dest
+    def transform(self, matrix: Transformation3d):
+        """Apply transformation to both endpoints (via Matrix inheritance)."""
+        self.src.transform(matrix)
+        self.dest.transform(matrix)
 
-    def __str__(self) -> str:
-        return f"Line3d({self.src} -> {self.dest})"
+    def __str__(self):
+        return f"Line3d[{self.src} -> {self.dest}]"
