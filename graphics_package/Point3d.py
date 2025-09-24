a9c3d823499b1799f9f5baaf048e71f4135db690
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-24 12:56:11 +0200

    Update Point3d.py

--- graphics_package/Point3d.py
+++ graphics_package/Point3d.py
@@ -1,25 +1,26 @@
+# graphics_package/point3d.py
+from graphics_package.matrix import Matrix
+from graphics_package.vector4d import Vector4d
 
-class Point3d:
-    """3D point with x, y, z coordinates."""
+class Point3d(Matrix):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        super().__init__(4, 1, 0.0)
+        self.m[0][0] = x
+        self.m[1][0] = y
+        self.m[2][0] = z
+        self.m[3][0] = 1.0  # homogeneous coord
 
-    def __init__(self, x: float, y: float, z: float):
-        self._x = x
-        self._y = y
-        self._z = z
+    @classmethod
+    def from_vector(cls, v: Vector4d):
+        return cls(v.x(), v.y(), v.z())
 
-    def x(self) -> float: return self._x
-    def y(self) -> float: return self._y
-    def z(self) -> float: return self._z
+    def x(self): return self.m[0][0]
+    def y(self): return self.m[1][0]
+    def z(self): return self.m[2][0]
 
-    def set_x(self, val: float) -> None: self._x = val
-    def set_y(self, val: float) -> None: self._y = val
-    def set_z(self, val: float) -> None: self._z = val
+    def set_x(self, val): self.m[0][0] = val
+    def set_y(self, val): self.m[1][0] = val
+    def set_z(self, val): self.m[2][0] = val
 
-    def as_tuple(self) -> tuple[float, float, float]:
-        return (self._x, self._y, self._z)
-
-    def transform(self, matrix: "Transformation3d") -> "Point3d":
-        return matrix.apply_point(self)
-
-    def __str__(self) -> str:
-        return f"{self._x} {self._y} {self._z}"
+    def __str__(self):
+        return f"({self.x():.3f}, {self.y():.3f}, {self.z():.3f})"
