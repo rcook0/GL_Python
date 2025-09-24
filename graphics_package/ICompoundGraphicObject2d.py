f8ced0f3be888ff9ce6ae2838f055c763461ec4c
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-24 21:41:27 +0200

    Update ICompoundGraphicObject2d.py

--- graphics_package/ICompoundGraphicObject2d.py
+++ graphics_package/ICompoundGraphicObject2d.py
@@ -2,7 +2,15 @@ from abc import ABC, abstractmethod
 from .IGraphicObject import IGraphicObject
 
 class ICompoundGraphicObject2d(IGraphicObject, ABC):
+    """
+    Abstract interface for compound 2D graphic objects.
+    Extends IGraphicObject.
+    """
+
     @abstractmethod
-    def add(self, obj: IGraphicObject) -> None: ...
+    def add(self, obj: IGraphicObject) -> None:
+        pass
+
     @abstractmethod
-    def clear(self) -> None: ...
+    def clear(self) -> None:
+        pass
