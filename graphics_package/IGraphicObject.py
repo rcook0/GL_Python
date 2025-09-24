76893578abd83f5545235797b8dbda603cce96fb
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-24 21:40:45 +0200

    Update IGraphicObject.py

--- graphics_package/IGraphicObject.py
+++ graphics_package/IGraphicObject.py
@@ -1,9 +1,20 @@
+# graphics_package/IGraphicObject.py
+
 from abc import ABC, abstractmethod
 
 class IGraphicObject(ABC):
+    """
+    Abstract base class for all graphic objects.
+    """
+
     @abstractmethod
-    def draw(self, g=None) -> None: ...
+    def draw(self, g=None) -> None:
+        pass
+
     @abstractmethod
-    def erase(self, g=None) -> None: ...
+    def erase(self, g=None) -> None:
+        pass
+
     @abstractmethod
-    def transform(self, trans) -> None: ...
+    def transform(self, trans) -> None:
+        pass
