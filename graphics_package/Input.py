8e3e8ded1342e9ed1618d026c11278ef4d60535c
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-24 21:37:00 +0200

    Update Input.py

--- graphics_package/Input.py
+++ graphics_package/Input.py
@@ -1,21 +1,17 @@
-# graphics_package/Buffer.py
-class Buffer:
+# graphics_package/Input.py
+class Input:
     """
-    Simple 2D buffer for integer pixel values.
+    Minimal Input utility to wrap Python's input().
     """
 
-    def __init__(self, width: int, height: int, init: int = 0):
-        self.width = width
-        self.height = height
-        self.data = [[init for _ in range(width)] for _ in range(height)]
+    @staticmethod
+    def read_line(prompt: str = "") -> str:
+        return input(prompt)
 
-    def clear(self, value: int = 0) -> None:
-        for y in range(self.height):
-            for x in range(self.width):
-                self.data[y][x] = value
+    @staticmethod
+    def read_int(prompt: str = "") -> int:
+        return int(input(prompt))
 
-    def set(self, x: int, y: int, value: int) -> None:
-        self.data[y][x] = value
-
-    def get(self, x: int, y: int) -> int:
-        return self.data[y][x]
+    @staticmethod
+    def read_float(prompt: str = "") -> float:
+        return float(input(prompt))
