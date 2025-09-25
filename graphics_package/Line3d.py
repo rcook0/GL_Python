9d40199d808c31684fa359745b8bf24efa1f3ed7
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-25 09:34:16 +0200

    Update Line3d.py

--- graphics_package/Line3d.py
+++ graphics_package/Line3d.py
@@ -1,18 +1,12 @@
-# graphics_package/line3d.py
 from graphics_package.point3d import Point3d
 from graphics_package.vector4d import Vector4d
 from graphics_package.transformation3d import Transformation3d
 from graphics_package.line2d import Line2d
+from graphics_package.igraphic_object3d import IGraphicObject3d
 
-class Line3d:
+class Line3d(IGraphicObject3d):
     def __init__(self, *args):
-        """
-        Constructors:
-        - Line3d(x1, y1, z1, x2, y2, z2)
-        - Line3d(Point3d, Point3d)
-        - Line3d(Vector4d, Vector4d)
-        """
-        if len(args) == 6:  # raw coords
+        if len(args) == 6:  # coords
             x1, y1, z1, x2, y2, z2 = args
             self.src = Point3d(x1, y1, z1)
             self.dest = Point3d(x2, y2, z2)
@@ -25,20 +19,15 @@ class Line3d:
         else:
             raise TypeError("Invalid constructor arguments for Line3d")
 
-    def get_source_point(self) -> Point3d:
-        return self.src
-
-    def get_destination_point(self) -> Point3d:
-        return self.dest
-
-    def line2d(self) -> Line2d:
-        """Project to 2D by stripping z-coordinate."""
-        return Line2d(self.src.x(), self.src.y(), self.dest.x(), self.dest.y())
+    def get_source_point(self): return self.src
+    def get_destination_point(self): return self.dest
 
     def transform(self, matrix: Transformation3d):
-        """Apply transformation to both endpoints (via Matrix inheritance)."""
         self.src.transform(matrix)
         self.dest.transform(matrix)
+
+    def to_2d(self):
+        return [Line2d(self.src.x(), self.src.y(), self.dest.x(), self.dest.y())]
 
     def __str__(self):
         return f"Line3d[{self.src} -> {self.dest}]"
